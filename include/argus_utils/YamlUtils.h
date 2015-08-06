ff3c1d9cd406764099ad7c12614ce4d97e12a925
Humphrey Hu <humhu@cmu.edu>
2015-08-06 13:24:41 -0400

    Moved argus_common to new package argus_utils.

---
+++ include/argus_utils/YamlUtils.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <yaml-cpp/yaml.h>
+
+#include <Eigen/Geometry>
+
+#include "argus_utils/PoseSE3.h"
+
+namespace argus_utils
+{
+	
+	/*! \brief Read/Write a PoseSE3 object to the node */
+	YAML::Node SetPoseYaml( const PoseSE3& pose );
+	bool GetPoseYaml( const YAML::Node& node, PoseSE3& pose );
+	
+	/*! \brief Read/Write a Quaternion object to the node */
+	YAML::Node SetQuaternionYaml( const Eigen::Quaterniond& quat );
+	bool GetQuaternionYaml( const YAML::Node& node, Eigen::Quaterniond& quat );
+	
+	/*! \brief Read/Write a Position object to the node */
+	YAML::Node SetPositionYaml( const Eigen::Translation3d& trans );
+	bool GetPositionYaml( const YAML::Node& node, Eigen::Translation3d& trans );
+	
+	/*! \brief Read/Write a matrix to the node */
+	YAML::Node SetMatrixYaml( const Eigen::MatrixXd& mat, 
+							  std::string idDim = "dimensions", std::string idVal = "values" );
+	bool GetMatrixYaml( const YAML::Node& node, Eigen::MatrixXd& mat, 
+						std::string idDim = "dimensions", std::string idVal = "values" );
+	
+}
