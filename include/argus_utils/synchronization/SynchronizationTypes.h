ef1a6e2b71b26cc21d3348f0dd1f558a6619deb9
Humphrey Hu <humhu@cmu.edu>
2017-03-17 12:33:54 -0400

    Added function to check external lock ownership

--- include/argus_utils/synchronization/SynchronizationTypes.h
+++ include/argus_utils/synchronization/SynchronizationTypes.h
@@ -2,15 +2,26 @@
 
 #include <boost/thread/thread.hpp>
 #include <boost/thread/locks.hpp>
+#include <boost/thread/lockable_adapter.hpp>
 
 namespace argus
 {
-	// TODO Move semaphore into here?
-	
-	typedef boost::shared_mutex Mutex;
-	typedef boost::shared_lock<Mutex> ReadLock;
-	typedef boost::unique_lock<Mutex> WriteLock;
-	
-	typedef boost::condition_variable_any ConditionVariable;
-	
+
+// TODO Move semaphore into here?
+
+typedef boost::shared_mutex Mutex;
+typedef boost::shared_lock<Mutex> ReadLock;
+typedef boost::unique_lock<Mutex> WriteLock;
+
+typedef boost::condition_variable_any ConditionVariable;
+
+template<template<typename> class Lock, typename Lockable>
+void CheckLockOwnership( const Lock<Lockable>& lock, const Lockable* lockable )
+{
+	if( lock.mutex() != lockable )
+	{
+		throw std::runtime_error( "Lock ownership error!" );
+	}
+}
+
 }
