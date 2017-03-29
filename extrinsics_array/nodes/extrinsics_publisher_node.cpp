d1fee688f6e12844cbe88accee2e76243c1d495b
Humphrey Hu <humhu@cmu.edu>
2017-03-29 13:36:03 -0400

    Updates to extrinsics array interface, refactor of argus_utils WIP

--- extrinsics_array/nodes/extrinsics_publisher_node.cpp
+++ extrinsics_array/nodes/extrinsics_publisher_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
-#include <tf2_ros/static_transform_broadcaster.h>
+
+#include "extrinsics_array/ExtrinsicsInterface.h"
 
 #include <argus_utils/geometry/GeometryUtils.h>
 #include <argus_utils/utils/ParamUtils.h>
@@ -11,10 +12,8 @@ int main( int argc, char** argv )
 {
 	ros::init( argc, argv, "extrinsics_publisher_node" );
 	ros::NodeHandle nh, ph( "~" );
-	tf2_ros::StaticTransformBroadcaster tfBroadcaster;
-
-	geometry_msgs::TransformStamped msg;
-	msg.header.stamp = ros::Time::now();
+	
+	ExtrinsicsInterface interface( nh );
 
 	YAML::Node transforms;
 	GetParamRequired( ph, "", transforms );
@@ -23,17 +22,16 @@ int main( int argc, char** argv )
 	{
 		// TODO For some reason this segfaults if it's a const &
 		YAML::Node info = iter->second;
-
-		GetParamRequired( info, "parent_id", msg.header.frame_id );
-		msg.child_frame_id = iter->first.as<std::string>();
+		std::string child = iter->first.as<std::string>();
+		std::string parent;
 		PoseSE3 pose;
+		GetParamRequired( info, "parent_id", parent );
 		GetParamRequired( info, "pose", pose );
-		msg.transform = PoseToTransform( pose );
 		
-		ROS_INFO_STREAM( "Publishing extrinsics for: " << msg.child_frame_id << 
-		                 " relative to " << msg.header.frame_id <<
+		ROS_INFO_STREAM( "Publishing extrinsics for: " << child << 
+		                 " relative to " << parent <<
 		                 " of " << pose );
-		tfBroadcaster.sendTransform( msg );
+		interface.SetStaticExtrinsics( child, parent, pose );
 	}
 
 	ros::spin();
