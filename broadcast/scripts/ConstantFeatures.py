4ec5c9caf5894f021a924251867b93a4441af52d
Humphrey Hu <humhu@cmu.edu>
2017-04-05 16:13:36 -0400

    Updated constant features node

--- broadcast/scripts/ConstantFeatures.py
+++ broadcast/scripts/ConstantFeatures.py
@@ -1,52 +1,51 @@
 #!/usr/bin/env python
 
 import rospy
-from broadcast.msg import StampedFeatures
-from broadcast import Broadcasters
+import broadcast
 
 class ConstantFeatures:
     '''Publishes a constant feature at a fixed rate or caches
        a constant feature for all time.'''
 
-    def __init__( self ):
-        rospy.init_node( 'constant_feature_publisher' )
-        
-        stream_name = rospy.get_param( '~stream_name' )
-        descriptions = rospy.get_param( '~descriptions' )
-        self.features = rospy.get_param( '~features' )
-        self.mode = rospy.get_param( '~mode' )
+    def __init__(self):
+        rospy.init_node('constant_feature_publisher')
+
+        stream_name = rospy.get_param('~stream_name')
+        description = rospy.get_param('~description')
+        self.features = rospy.get_param('~features')
+        self.mode = rospy.get_param('~mode')
 
         opt_args = {}
         if self.mode == 'push':
-            opt_args['queue_size'] = rospy.get_param( '~queue_size', 0 )
-            self.publish_rate = rospy.Rate( rospy.get_param( '~publish_rate' ) )
+            opt_args['queue_size'] = rospy.get_param('~queue_size', 0)
+            self.publish_rate = rospy.Rate(rospy.get_param('~publish_rate'))
         elif self.mode == 'pull':
             opt_args['cache_time'] = float('inf')
         else:
-            raise ValueError( 'Mode must be pull or push' )
+            raise ValueError('Mode must be pull or push')
 
-        rate = rospy.get_param( '~publish_rate', 1.0 )
-        self.publish_rate = rospy.Rate( rate );
+        rate = rospy.get_param('~publish_rate', 1.0)
+        self.publish_rate = rospy.Rate(rate)
 
-        self.feat_tx = Broadcasters.Transmitter( stream_name = stream_name,
-                                                 namespace = '~',
-                                                 feature_size = len( self.features ),
-                                                 descriptions = descriptions,
-                                                 mode = self.mode,
-                                                 **opt_args )
+        self.feat_tx = broadcast.Transmitter(stream_name=stream_name,
+                                             namespace='~',
+                                             feature_size=len(self.features),
+                                             description=description,
+                                             mode=self.mode,
+                                             **opt_args)
 
-    def Execute( self ):
-
+    def Execute(self):
         if self.mode == 'push':
-            
+
             while not rospy.is_shutdown():
-                self.feat_tx.publish( rospy.Time.now(), self.features )
+                self.feat_tx.publish(rospy.Time.now(), self.features)
                 self.publish_rate.sleep()
         else:
-            self.feat_tx.publish( rospy.Time(0), self.features )
+            self.feat_tx.publish(rospy.Time(0), self.features)
             # NOTE: ROS doesn't like printing numbers larger than 4294967295
-            self.feat_tx.publish( rospy.Time(4294967295), self.features ) 
+            self.feat_tx.publish(rospy.Time(4294967295), self.features)
             rospy.spin()
+
 
 if __name__ == '__main__':
     try:
@@ -54,5 +53,3 @@ if __name__ == '__main__':
         of.Execute()
     except rospy.ROSInterruptException:
         pass
-
-
